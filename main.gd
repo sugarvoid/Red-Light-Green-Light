6a33df875b2356ae06d0ef5a89d1ba597431b64a
Dave <gamedev@kitargo.com>
2022-11-27 17:25:35 -0600

    Add traffic light png. Add separate red/green times.

--- main.gd
+++ main.gd
@@ -3,10 +3,15 @@ extends Control
 
 onready var timer: Timer = get_node("Timer")
 onready var btn_start: Button = get_node("BtnStart")
-onready var txt_min_sec: TextEdit = get_node("TxtMinSeconds")
-onready var txt_max_sec: TextEdit = get_node("TxtMaxSeconds")
+
+onready var txt_green_min: TextEdit = get_node("TxtGreenMin")
+onready var txt_green_max: TextEdit = get_node("TxtGreenMax")
+
+onready var txt_red_min: TextEdit = get_node("TxtRedMin")
+onready var txt_red_max: TextEdit = get_node("TxtRedMax")
+
 onready var txt_rounds: TextEdit = get_node("TxtRounds")
-onready var color_square: ColorRect = get_node("ModeColorRect")
+onready var light: Sprite = get_node("Light")
 
 
 enum STATES {
@@ -18,8 +23,10 @@ enum STATES {
 var state: int = STATES.NOT_PLAYING
 var total_rounds: int
 var current_round: int
-var min_sec: float
-var max_sec: float
+var green_min: float
+var green_max: float
+var red_min: float
+var red_max: float
 var rng: RandomNumberGenerator
 
 
@@ -33,7 +40,7 @@ func _start_new_round():
 	self.state = STATES.GREEN
 	_update_square_color()
 	rng.randomize()
-	var round_length: float = rng.randf_range(min_sec, max_sec)
+	var round_length: float = rng.randf_range(red_min, red_max)
 	print(str('Round ', self.current_round))
 	print(str('Green for ', round_length, ' seconds'))
 	self.timer.start(round_length)
@@ -43,18 +50,20 @@ func _start_rest_mode():
 	self.state = STATES.RED
 	_update_square_color()
 	rng.randomize()
-	var round_length: float = rng.randf_range(min_sec, max_sec)
+	var round_length: float = rng.randf_range(green_min, green_max)
 	print(str('Red for ', round_length, ' seconds'))
 	self.timer.start(round_length)
 
 
 func _start_game():
-	if self.txt_max_sec.text.empty() || self.txt_min_sec.text.empty() || self.txt_rounds.text.empty():
+	if self.txt_green_max.text.empty() || self.txt_green_min.text.empty() || self.txt_rounds.text.empty():
 		return
 	
 	self.current_round = 1
-	self.min_sec = int(txt_min_sec.text)
-	self.max_sec = int(txt_max_sec.text)
+	self.red_min = int(txt_red_min.text)
+	self.red_max = int(txt_red_max.text)
+	self.green_min = int(txt_green_min.text)
+	self.green_max = int(txt_green_max.text)
 	self.total_rounds = int(txt_rounds.text)
 	
 	self._start_new_round()
@@ -65,7 +74,6 @@ func _on_timer_timeout():
 			current_round += 1
 			self._start_rest_mode()
 		else:
-			print('game over')
 			state = STATES.NOT_PLAYING
 			self._clear_txt_boxes()
 			self.timer.stop()
@@ -75,15 +83,17 @@ func _on_timer_timeout():
 		
 
 func _clear_txt_boxes() -> void:
-	self.txt_max_sec.text = ""
-	self.txt_min_sec.text = ""
+	self.txt_green_min.text = ""
+	self.txt_green_max.text = ""
+	self.txt_red_min.text = ""
+	self.txt_red_max.text = ""
 	self.txt_rounds.text = ""
 
 func _update_square_color():
 	match self.state:
 		STATES.GREEN:
-			self.color_square.color = Color.green
+			self.light.frame = 2
 		STATES.RED:
-			self.color_square.color = Color.red
+			self.light.frame = 1
 		STATES.NOT_PLAYING:
-			self.color_square.color = Color.whitesmoke
+			self.light.frame = 0
