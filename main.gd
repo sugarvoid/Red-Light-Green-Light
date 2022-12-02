0bba7b17ced10322aec054f3d71db0cd755e41b6
Dave <gamedev@kitargo.com>
2022-12-01 19:27:32 -0600

    Change from round based to time based.

--- main.gd
+++ main.gd
@@ -1,13 +1,13 @@
 extends Control
 
-
+onready var tmr_main: Timer = get_node("TmrMain")
 onready var go_time: ModeTime = get_node("GoTime")
 onready var rest_time: ModeTime = get_node("StopTime")
 
 onready var timer: Timer = get_node("Timer")
 onready var btn_start: Button = get_node("BtnStart")
 
-onready var txt_rounds: TextEdit = get_node("TxtRounds")
+onready var txt_game_min: TextEdit = get_node("TxtRounds")
 onready var light: Sprite = get_node("Light")
 
 
@@ -18,13 +18,14 @@ enum STATES {
 }
 
 var state: int = STATES.NOT_PLAYING
-var total_rounds: int
 var current_round: int
 var rng: RandomNumberGenerator
+var game_length: int
 
 
 func _ready():
 	self.rng = RandomNumberGenerator.new()
+	self.tmr_main.connect("timeout", self, "_game_over")
 	self.timer.connect("timeout", self, "_on_timer_timeout")
 	self.btn_start.connect("pressed", self, "_start_game")
 
@@ -33,7 +34,6 @@ func _start_new_round():
 	self.state = STATES.GREEN
 	_update_square_color()
 	var round_length: int = self._get_round_length(go_time.get_min_time(), go_time.get_max_time()) 
-	print(str('Round ', self.current_round))
 	print(str('Green for ', round_length, ' seconds'))
 	self.timer.start(round_length)
 
@@ -49,27 +49,21 @@ func _start_rest_mode():
 func _start_game():
 	if self.go_time.has_blank_spots() || self.rest_time.has_blank_spots():
 		return
-	self.current_round = 1
-	self.total_rounds = int(txt_rounds.text)
+	self.game_length = int(txt_game_min.text) * 60
+	self.tmr_main.start(game_length)
 	self._start_new_round()
 
 
 func _on_timer_timeout():
 	if self.state == STATES.GREEN:
-		if self.current_round < total_rounds:
-			self.current_round += 1
-			self._start_rest_mode()
-		else:
-			self.state = STATES.NOT_PLAYING
-			self._clear_txt_boxes()
-			self.timer.stop()
-			_update_square_color()
+		self._start_rest_mode()
 	elif self.state == STATES.RED:
 		self._start_new_round()
 		
 
 func _clear_txt_boxes() -> void:
-	self.txt_rounds.text = ""
+	print('game over')
+	self.txt_game_min.text = ""
 	self.go_time.clear_text_edits()
 	self.rest_time.clear_text_edits()
 
@@ -86,3 +80,9 @@ func _update_square_color():
 			self.light.frame = 1
 		STATES.NOT_PLAYING:
 			self.light.frame = 0
+
+func _game_over():
+	self.state = STATES.NOT_PLAYING
+	self._clear_txt_boxes()
+	self.timer.stop()
+	_update_square_color()
